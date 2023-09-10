381dda43a0a31f44c5a1fde3319d197a2434b0e9
Carlos Javier <carlos21201068@gmail.com>
2023-09-09 21:31:47 -0300

    se crea pagina de error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
-
+import "./App.css";
+import { Error404 } from "./components/views/Error404";
 function App() {
-
   return (
     <>
-
+      <Error404 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
