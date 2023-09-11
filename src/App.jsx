b8b6650194297b1d91915920cdf1b29fcb237405
Carlos Javier <carlos21201068@gmail.com>
2023-09-11 20:45:39 -0300

    se crea estructura basica de componente administrador y sus funciones

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Login from "./components/Login";
 import { Error404 } from "./components/views/Error404";
 import { Contacto } from "./components/views/Contacto";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Administrador from "./components/views/Administrador";
+
 function App() {
   return (
     <>
+      <Administrador></Administrador>
       <Navegacion></Navegacion>
       <Login></Login>
       <Footer></Footer>
